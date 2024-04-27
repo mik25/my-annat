09aa1648f0404f1adcd30544f09823c42b3f6a74
mik25 <mikmc55@gmail.com>
2024-04-27 03:18:16 +0100

    Update jackett.py

--- annatar/jackett.py
+++ annatar/jackett.py
@@ -23,8 +23,8 @@ from annatar.torrent import Category
 
 log = structlog.get_logger(__name__)
 
-JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://1.14.73.37:9117")
-JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "ft950g440swlqnrk8vpu35s533ok1fo8")
+JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://87.98.218.210:9117")
+JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "5mprcfoumvjvn1yv9o7lt89yx51jod7z")
 
 JACKETT_MAX_RESULTS = int(os.environ.get("JACKETT_MAX_RESULTS", 100))
 JACKETT_TIMEOUT = int(os.environ.get("JACKETT_TIMEOUT", 6))
@@ -34,8 +34,9 @@ JACKETT_CACHE_MINUTES = timedelta(minutes=int(os.environ.get("JACKETT_CACHE_MINU
 
 JACKETT_INDEXERS_LIST: list[str] = os.environ.get(
     "JACKETT_INDEXERS",
-    "btsow,hdarea,hdatmos,hdfans,monikadesign-api,passthepopcorn,thesceneplace,torlock,xspeeds",
+    "1337x,Bittorrentfiles,fearnopeer,filelist,ncore,onlyencodes-api,pretome,resurrectthenet,scenehd,skipthecommercials-api,solidtorrents,speedapp,torrentdownloads,torrentday,torrentproject2",
 ).split(",")
+
 
 REQUEST_DURATION = Histogram(
     name="jackett_request_duration_seconds",
